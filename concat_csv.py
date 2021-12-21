4184e674e9e9f63c0032e82febbf85e2b269791a
Arthur Arnx <36545443+Arnouux@users.noreply.github.com>
2021-12-21 14:38:51 +0100

    more preprocessing

--- concat_csv.py
+++ concat_csv.py
@@ -35,7 +35,7 @@ def clean_sentence(sentence):
         sentence[sentence.index(word)] = word_best
     return " ".join(sentence)
         
-
+sentences = {}
 train = []
 x = 0
 with open(FINAL, "w", encoding="utf-8") as f:
@@ -43,11 +43,24 @@ with open(FINAL, "w", encoding="utf-8") as f:
         line = f2.readline()
         while line != "":
             #### preprocessing
-            line = line.replace("<user>", "")
-            line = line.replace("<url>", "")
+            # line = line.replace("<user>", "")
+            # line = line.replace("<url>", "")
+            # line = line.replace("<url>", "")
+            line = line.replace("u2", "you too")
+            line = line.replace("u 2", "you too")
+            line = line.replace("you 2", "you too")
+            line = line.replace("you2", "you too")
+            line = line.replace("<3", "love")
+            line = re.sub("[0-9]", "<number>", line)
             line = " ".join(line.split())
             #line = clean_sentence(line)
             ####
+            if line in sentences:
+                line = f2.readline()
+                continue
+            else:
+                sentences[line] = True
+            
             if len(line) > 1:
                 try:
                     f.write(f"{line[:-1]}\t0\n")
@@ -55,17 +68,30 @@ with open(FINAL, "w", encoding="utf-8") as f:
                     pass
             line = f2.readline()
             x += 1
-            if x % 10 == 0:
+            if x % 100000 == 0:
                 print(x)
     with open(PATH_POS, "r", encoding="utf-8") as f2:
         line = f2.readline()
         while line != "":
             #### preprocessing
-            line = line.replace("<user>", "")
-            line = line.replace("<url>", "")
+            # line = line.replace("<user>", "")
+            # line = line.replace("<url>", "")
+            # line = line.replace("<url>", "")
+            line = line.replace("u2", "you too")
+            line = line.replace("u 2", "you too")
+            line = line.replace("you 2", "you too")
+            line = line.replace("you2", "you too")
+            line = line.replace("<3", "love")
+            line = re.sub("[0-9]", "<number>", line)
             line = " ".join(line.split())
             #line = clean_sentence(line)
             ####
+            if line in sentences:
+                line = f2.readline()
+                continue
+            else:
+                sentences[line] = True
+                
             if len(line) > 1:
                 try:
                     f.write(f"{line[:-1]}\t1\n")
@@ -90,3 +116,28 @@ with open(FINAL, "r", encoding="utf-8") as f:
     with open(FINAL_TEST, "w", encoding="utf-8") as f2:
         for line in lines[int(len(lines)*0.8):]:
             f2.write(f"{line[:-1]}\n")
+            
+#####test_data     
+x = 0
+from tqdm import tqdm
+with tqdm(total=100) as pbar:
+    with open("test_data.txt", "r", encoding="utf-8") as f2:
+        with open("test_data_processed.txt", "w", encoding="utf-8") as f:
+            line = f2.readline()
+            while line != "":
+                line = line.split(',', 1)
+                #### preprocessing
+                line[1] = line[1].replace("u2", "you too")
+                line[1] = line[1].replace("u 2", "you too")
+                line[1] = line[1].replace("you 2", "you too")
+                line[1] = line[1].replace("you2", "you too")
+                line[1] = line[1].replace("<3", "love")
+                line[1] = re.sub("[0-9]", "<number>", line[1])
+                line = line[0] + "," + " ".join(line[1].split())
+                #line = clean_sentence(line)
+                ####
+                f.write(f"{line[:-1]}\n")
+                line = f2.readline()
+                x += 1
+                if x % 100 == 0:
+                    pbar.update(1)
